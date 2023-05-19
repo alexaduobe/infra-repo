55c352ca144ad7df7bcb0981d339872b7675ce8d
alexaduobe <alex.aduobe@gmail.com>
2023-05-19 10:45:09 -0500

    new commit

---
+++ aws-infra/terraform/stacks/ou-nonsso/variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+  description = "AWS CLI profile to use"
+  type        = string
+}
+
+variable "region" {
+  description = "Default region for resources creation"
+  default     = "us-east-2"
+  type        = string
+}
+
